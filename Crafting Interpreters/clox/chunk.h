127280e0554b411a047c78b904c12ca4b0a6e90b
MaruLin <how7633818@gmail.com>
2024-02-20 11:12:41 +0800

    create branch for challenge 14

--- Crafting Interpreters/clox/chunk.h
+++ Crafting Interpreters/clox/chunk.h
@@ -6,11 +6,6 @@
 
 typedef enum {
     OP_CONSTANT,
-    OP_ADD,
-    OP_SUBTRACT,
-    OP_MULTIPLY,
-    OP_DIVIDE,
-    OP_NEGATE,
     OP_RETURN,
 } OpCode;
 
@@ -24,6 +19,6 @@ typedef struct {
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte, int line);
+void wtireChunk(Chunk* chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
 #endif
